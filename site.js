647cf5a3a07b2f02a59401f4afba54fed3bfaf45
Barchard <lb185070@ncr.com>
2017-03-18 11:58:59 -0400

    hooking up the button

--- site.js
+++ site.js
@@ -1,6 +1,3 @@
-/*jslint node:false*/
-/*browser:true*/
-
 $( document ).ready( function () {
 	window.fbAsyncInit = function() {
     FB.init({
@@ -8,6 +5,14 @@ $( document ).ready( function () {
       xfbml      : true,
       version    : 'v2.8'
     });
+
+	(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk')); 
 
 	// "use strict";
 	//get rid of all that old stuff
@@ -29,8 +34,4 @@ $( document ).ready( function () {
 			href: 'https://developers.facebook.com/docs/'
 			}, function(response){});
   	});
-   
-	
-
-
 });
