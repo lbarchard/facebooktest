51ae2be37bce628bc8c259cdd5669f50ccf86e60
Barchard <lb185070@ncr.com>
2017-03-18 11:38:54 -0400

    hooking up the button

--- site.js
+++ site.js
@@ -7,10 +7,6 @@ $( document ).ready( function () {
 	//window.location = "http://gt.bootcampcontent.com/pineapplesoda/project-one";
 	//redirect to gitlab readme until we actually have something to serve up
 
-$('#add-user').on("click", function(event){
-  	event.preventDefault();
-
-
 	$("#facebook_button").on("click") function(d, s, id){
      var js, fjs = d.getElementsByTagName(s)[0];
      if (d.getElementById(id)) {return;}
